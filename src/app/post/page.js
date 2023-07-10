a8ca717eaded3c011195ff15c56be15f11be8a14
michaeljymsgutierrez <leeez711@gmail.com>
2023-07-10 16:28:56 +0800

    Play around model, view, controller

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -6,22 +6,24 @@ import useShopController from "./useShopController";
 let counter = 0;
 
 function PostsPage() {
-  const { posts, users } = useShopRoute();
-  const controllerModel = useShopController();
+  const model = useShopRoute();
+  const controller = useShopController(model);
 
-  if (!posts) return <h1>Loading...</h1>;
+  if (!controller.posts) return <h1>Loading...</h1>;
 
   console.log("counting re-renders", (counter += 1));
 
   return (
     <>
-      <h2>Clicked Post: {controllerModel.clickedPostId}</h2>
+      <span>{controller.posts.length}</span>
+      <h2>Clicked Post: {controller.clickedPostId}</h2>
+      <button onClick={controller.removePost}>Remove Post</button>
       <h1>Posts Page</h1>
-      {posts.map((post, index) => (
+      {controller.posts.map((post, index) => (
         <div key={index}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
-          <button onClick={() => controllerModel.viewPostId(post.id)}>
+          <button onClick={() => controller.viewPostId(post.id)}>
             View ID
           </button>
           <hr />
--- src/app/post/useShopController.js
+++ src/app/post/useShopController.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
 
-function useShopController() {
+function useShopController(props) {
+  const [posts, setPosts] = useState(props.posts || [])
   const [clickedPostId, setClickedPostId] = useState(null);
+
+  setPosts(props.posts)
+
+  console.log(props.posts)
+  console.log(posts)
 
   function viewPostId(postId) {
     setClickedPostId(postId);
   }
 
-  return { clickedPostId, viewPostId };
+  function removePost() {
+    props.posts.pop()
+    // const updatePosts = posts.pop()
+    // posts = updatedPosts
+  }
+
+  return { clickedPostId, viewPostId, posts };
 }
 
 export default useShopController;
